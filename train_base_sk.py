5851c9c6a73a8e99493a6aa7d3c6013640eb458f
03bennej <jjrbenn@gmail.com>
2022-03-22 09:50:25 -0400

    Generalise parallelization

--- train_base_sk.py
+++ train_base_sk.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('--node', '-N', type=str, default='16', help='number of node requested')
     parser.add_argument('--time', '-T', type=str, default='20:00', help='number of hours requested')
     parser.add_argument('--memory', '-M', type=str, default='20000', help='memory requested in MB')
-    parser.add_argument('--hpc', type=str, default='minerva', help='parallelize using joblib or hpc')
+    parser.add_argument('--hpc', type=str, default='minerva', help='parallelize using minerva or other')
     parser.add_argument('--fold', '-F', type=int, default=5, help='number of cross-validation fold')
     parser.add_argument('--rank', type=str2bool, default='False', help='getting attribute importance')
     # parser.add_argument('--create_rank_dir', type=str2bool, default='False', help='getting attribute importance')
@@ -176,5 +176,5 @@ if __name__ == "__main__":
         system('sh %s' % jobs_fn)
         system('rm %s' % jobs_fn)
     end = time()
-    if not args.parallel == 'hpc':
+    if not args.parallel == 'minerva':
         print('Elapsed time is: %s seconds' % (end - start))
