941a2d9f28ec67a2f472293a564ac72bf7e88cc5
03bennej <jjrbenn@gmail.com>
2022-03-22 16:03:15 -0400

    Generalise parallelization

--- train_base_sk.py
+++ train_base_sk.py
@@ -101,6 +101,7 @@ if __name__ == "__main__":
     predictors = {}
     with open(predictors_path) as f:
         for line in f:
+            print(line)
             if line.startswith('#'):
                 continue
             else:
