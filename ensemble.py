b45a201cb0e8efbe2a7d2b7c83120fee0efaac17
03bennej <jjrbenn@gmail.com>
2022-04-28 14:06:14 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -368,7 +368,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
                            alpha_nmf=1,
                            max_iter_nmf=500,
                            use_probs=False,
-                           nmf=False,
+                           nmf=True,
                            return_probs=True,
                            method="mean"),
         # "CF.S3": cf_stacker(base_estimator=LinearRegression(),
