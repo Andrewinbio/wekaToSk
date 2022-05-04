c247abe01b64064bd4b5cd2d7471e50e932bdd0f
03bennej <jjrbenn@gmail.com>
2022-05-04 10:43:29 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -365,7 +365,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
                            latent_dimension=500,
                            threshold=0.5,
                            alpha_nmf=0.75,
-                           max_iter_nmf=5000,
+                           max_iter_nmf=500,
                            tol_nmf=0.0001,
                            l1_ratio_nmf=0.0,
                            nmf=True,
