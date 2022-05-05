9660a202e809cdad3c63ee9bdeb44ecd120e7642
03bennej <jjrbenn@gmail.com>
2022-05-05 17:22:03 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -353,7 +353,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
         mkdir(analysis_path)
     """ Stacking Ensemble """
     stackers_dict = {
-        "CF.S-without-NMF": CFStacker(base_estimator=LinearRegression(),
+        "CF.S-without-NMF": CFStacker(base_estimator=LogisticRegression(),
                            latent_dimension=5,
                            threshold=0.6,
                            alpha_nmf=0.6,
@@ -361,7 +361,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
                            nmf=False,
                            return_probs=True,
                            method="mean"),
-        "CF.S-LR": CFStacker(base_estimator=LinearRegression(),
+        "CF.S-LR": CFStacker(base_estimator=LogisticRegression(),
                            latent_dimension=5,
                            threshold=0.6,
                            alpha_nmf=2,
