7129e1e55228c5091963a53a9236c460eb770283
03bennej <jjrbenn@gmail.com>
2022-05-05 11:38:51 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -364,7 +364,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
         "CF.S-LR": CFStacker(base_estimator=LinearRegression(),
                            latent_dimension=4,
                            threshold=0.5,
-                           alpha_nmf=1,
+                           alpha_nmf=0.5,
                            max_iter_nmf=10,
                            tol_nmf=0.0001,
                            l1_ratio_nmf=0.0,
