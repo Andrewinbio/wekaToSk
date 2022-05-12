dbf32e25e1bc097e572014c7c8cc831e282801e7
03bennej <jjrbenn@gmail.com>
2022-05-12 14:41:22 -0400

    minor change

--- common.py
+++ common.py
@@ -21,11 +21,11 @@ def str2bool(v):
 
 def generic_classifier_predict(clf, regression_bool, input_data):
     if hasattr(clf, "predict_proba") and (not regression_bool):
-        test_predictions = clf.predict_proba(input_data)[:, 1]
+        test_predictions = clf.predict_proba(input_data)#[:, 1]
     else:
         test_predictions = clf.predict(input_data)
         if regression_bool is False:
-            test_predictions = test_predictions#[:, 1]
+            test_predictions = test_predictions[:, 1]
 
     return test_predictions
 
