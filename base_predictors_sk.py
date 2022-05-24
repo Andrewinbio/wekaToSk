09a358976fc7efb2e7dab1a45ca6cb63d7b49bc2
03bennej <jjrbenn@gmail.com>
2022-05-24 16:56:36 -0400

    minor change

--- base_predictors_sk.py
+++ base_predictors_sk.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
 
     if not regression:
         y_ = data[classAttribute]
-        print(predictClassValue)
+        print('The class value is ', predictClassValue)
         y_.loc[~(y_ == predictClassValue)] = 0
         y_.loc[y_ == predictClassValue] = 1
         data[classAttribute] = y_.astype(int)
