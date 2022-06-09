53e202f9c9829124dc5954807338b5532815caca
03bennej <jjrbenn@gmail.com>
2022-06-09 16:03:38 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -358,7 +358,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
                                latent_dim=10,
                                matrix_factorization=True,
                                max_iter=50,
-                               tol=0.005,
+                               tol=0.01,
                                lamW=0.0,
                                lamH=0.0,
                                method="mean",
