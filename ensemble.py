c8371d992e1012f686319ab31d728d42522503f6
03bennej <jjrbenn@gmail.com>
2022-06-13 13:05:27 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -357,7 +357,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
         "CF.S-NMF1": MatrixFactorizationClassifier(latent_dim=10,
                                                    max_iter=200,
                                                    learning_rate=0.001,
-                                                   tol=0.01,
+                                                   tol=0.05,
                                                    lamW=0.0,
                                                    lamH=0.0),
         "RF.S": RandomForestClassifier(),
