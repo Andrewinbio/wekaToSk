7ff61bc6d14316988ad0f25dadcfbb2f7d1bbc53
03bennej <jjrbenn@gmail.com>
2022-06-18 12:13:20 -0400

    minor change

--- ensemble.py
+++ ensemble.py
@@ -354,7 +354,7 @@ def main_classification(path, f_list, agg=1, rank=False, ens_for_rank=''):
         mkdir(analysis_path)
     """ Stacking Ensemble """
     stackers_dict = {
-        "CF.S-NMF1": MatrixFactorizationClassifier(latent_dim=3,
+        "CF.S-NMF1": MatrixFactorizationClassifier(latent_dim=8,
                                                    max_iter=5000,
                                                    learning_rate=0.001,
                                                    tol=0.02,
